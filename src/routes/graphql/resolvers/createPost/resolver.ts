a6daced1313743a73190800ab395a2b99d67ce5b
Ivan Koliada <ivanKoliada69@gmail.com>
2023-01-27 22:03:55 +0300

    feat: validate create post input

--- src/routes/graphql/resolvers/createPost/resolver.ts
+++ src/routes/graphql/resolvers/createPost/resolver.ts
@@ -8,13 +8,18 @@ export const createPostResolver = {
       { input }: { input: Omit<PostEntity, "id"> },
       fastify: FastifyInstance
     ) => {
+      const user = await fastify.db.users.findOne({
+        key: "id",
+        equals: input.userId,
+      });
+
       const post = await fastify.db.posts.create(input);
 
-      if (post) {
+      if (user && post) {
         return post;
       }
 
-      throw fastify.httpErrors.badRequest();
+      throw fastify.httpErrors.badRequest("user does not exist");
     },
   },
 };
