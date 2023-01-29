89dc1c4bf6188300c31c622fd5355c7bff5bcd67
janChorny <janchorny@tut.by>
2023-01-29 23:38:46 +0300

    feat: add get all entities by id

--- src/routes/graphql/query.ts
+++ src/routes/graphql/query.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import { GraphQLList, GraphQLObjectType } from "graphql";
+import { GraphQLList, GraphQLObjectType, GraphQLString } from "graphql";
 import { memberType } from "./types/memberType";
 import { postType } from "./types/postType";
 import { profileType } from "./types/profileType";
@@ -14,11 +14,37 @@ export const generalQuery = new GraphQLObjectType({
         return await fastify.db.users.findMany();
       },
     },
+    user: {
+      type: userType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve: async (_: any, args: any, fastify: FastifyInstance) => {
+        const user = await fastify.db.users.findOne({ key: 'id', equals: args.id });
+        if (!user) {
+          throw fastify.httpErrors.notFound();
+        }
+        return user;
+      }
+    },
     profiles: {
       type: new GraphQLList(profileType),
       resolve: async (_: any, args: any, fastify: FastifyInstance) => {
         return await fastify.db.profiles.findMany();
       },
+    },
+    profile: {
+      type: profileType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve: async (_: any, args: any, fastify: FastifyInstance) => {
+        const profile = await fastify.db.profiles.findOne({ key: 'id', equals: args.id });
+        if (!profile) {
+          throw fastify.httpErrors.notFound();
+        }
+        return profile;
+      }
     },
     posts: {
       type: new GraphQLList(postType),
@@ -26,11 +52,37 @@ export const generalQuery = new GraphQLObjectType({
         return await fastify.db.posts.findMany();
       },
     },
+    post: {
+      type: postType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve: async (_: any, args: any, fastify: FastifyInstance) => {
+        const post = await fastify.db.posts.findOne({ key: 'id', equals: args.id });
+        if (!post) {
+          throw fastify.httpErrors.notFound();
+        }
+        return post;
+      }
+    },
     memberTypes: {
       type: new GraphQLList(memberType),
       resolve: async (_: any, args: any, fastify: FastifyInstance) => {
         return await fastify.db.memberTypes.findMany();
       },
     },
+    memberType: {
+      type: memberType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve: async (_: any, args: any, fastify: FastifyInstance) => {
+        const memberType = await fastify.db.memberTypes.findOne({ key: 'id', equals: args.id });
+        if(!memberType){
+          throw fastify.httpErrors.notFound();
+        }
+        return memberType
+      }
+    }
   }
 })
